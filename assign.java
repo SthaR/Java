2d4371c29b3c17db53ebc30688cb776637806ce6
Ruby Shrestha <ruby_0341@deerwalk.edu.np>
2015-01-15 21:23:55 +0545

    Astericks around

---
+++ assign.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Ruby on 1/15/2015.
+ */
+package com.company;
+
+public class assign {
+
+    public static void main(String[] args) {
+        int x, y;
+        for (x=0; x<5; x++)
+        {
+            for (y=0; y<=x; y++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
